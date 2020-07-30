9ef5cd4ecdc6d423b0788afd58996e9278f886f6
as.stromov <as.stromov@tensor.ru>
2020-07-30 09:31:14 +0300

    refactor (hidden js,map and css files) fix problem dnd terrain

--- Server/moleculer/services/EditorService/EditorService.service.ts
+++ Server/moleculer/services/EditorService/EditorService.service.ts
@@ -1,173 +1,181 @@
 import {globalVariables} from "../../../../Client/GlobalVariables";
+
 const Service = require("moleculer").Service;
 const ApiGateway = require("moleculer-web");
 const fs = require('fs');
 const path = require('path');
+
 class EditorService extends Service {
 
-    constructor(broker) {
-        super(broker);
+	constructor(broker) {
+		super(broker);
 
-        this.parseServiceSchema({
-            name: "EditorService",
-            mixins: [ApiGateway],
-            meta: {
-                scalable: true
-            },
-            settings: {
-                port: process.env.PORT || 3001,
-                upperCase: true,
+		this.parseServiceSchema({
+			name: "EditorService",
+			mixins: [ApiGateway],
+			meta: {
+				scalable: true
+			},
+			settings: {
+				port: process.env.PORT || 3001,
+				upperCase: true,
 
-                routes: [{
-                    path: "/",
-                    cors: {
-                        origin: [`${globalVariables.serverPath}:3001`, `${globalVariables.serverPath}:8080`],
-                        methods: ["GET", "OPTIONS", "POST"]
-                    },
-                    bodyParsers: {
-                        json: true,
-                        urlencoded: {extended: true}
-                    },
-                    aliases: {
-                        "GET /api/getProjectStructure": "EditorService.getProjectStructure",
-                        "POST /api/getInfoAboutStructure": 'EditorService.getInfoAboutStructure',
-                    },
-                    onBeforeCall(ctx, route, req, res) {
-                        ctx.meta.userAgent = req.headers["user-agent"];
+				routes: [{
+					path: "/",
+					cors: {
+						origin: [`${globalVariables.serverPath}:3001`, `${globalVariables.serverPath}:8080`],
+						methods: ["GET", "OPTIONS", "POST"]
+					},
+					bodyParsers: {
+						json: true,
+						urlencoded: {extended: true}
+					},
+					aliases: {
+						"GET /api/getProjectStructure": "EditorService.getProjectStructure",
+						"POST /api/getInfoAboutStructure": 'EditorService.getInfoAboutStructure',
+					},
+					onBeforeCall(ctx, route, req, res) {
+						ctx.meta.userAgent = req.headers["user-agent"];
 
-                    },
-                    // onAfterCall(ctx, route, req, res, data) {
-                    //     console.log('onAfterCall');
-                    //     return '123123';
-                    //
-                    // },
-                    onError(req, res, err) {
-                        console.log('Error route');
-                        res.setHeader("Content-Type", "text/plain");
-                        res.writeHead(err.code || 500);
-                        res.end("Route error GameEngineServer response: " + err.message);
-                    }
-                }]
-            },
-            actions: {
-                checkUserAuthorization: this.userAuthorization,
-                getProjectStructure: this.sendDirectoryEngine,
-                getInfoAboutStructure: this.sendInfoAboutStructure
-            },
-            events: {},
-            created: this.serviceCreated,
-            started: this.serviceStarted,
-            stopped: this.serviceStopped,
-        });
-    }
+					},
+					// onAfterCall(ctx, route, req, res, data) {
+					//     console.log('onAfterCall');
+					//     return '123123';
+					//
+					// },
+					onError(req, res, err) {
+						console.log('Error route');
+						res.setHeader("Content-Type", "text/plain");
+						res.writeHead(err.code || 500);
+						res.end("Route error GameEngineServer response: " + err.message);
+					}
+				}]
+			},
+			actions: {
+				checkUserAuthorization: this.userAuthorization,
+				getProjectStructure: this.sendDirectoryEngine,
+				getInfoAboutStructure: this.sendInfoAboutStructure
+			},
+			events: {},
+			created: this.serviceCreated,
+			started: this.serviceStarted,
+			stopped: this.serviceStopped,
+		});
+	}
 
-    /**
-     * Метод чтения запрошеного с клиента файла и отправки его клиенту
-     * @param request
-     * @param response
-     */
-    sendInfoAboutStructure(request, response): void {
-        const name = request.params.name;
-        const type = request.params.type;
-        const extension = request.params.extension;
-        const promise = new Promise((resolve, reject) => {
-            fs.readFile(request.params.path, "utf8", (error, data) => {
-                resolve(data);
-            });
-        });
-        request.options.parentCtx.params.res.writeHead(200, {'Content-Type': 'text/plain'});
-        promise.then((data) => {
-            try {
+	/**
+	 * Метод чтения запрошеного с клиента файла и отправки его клиенту
+	 * @param request
+	 * @param response
+	 */
+	sendInfoAboutStructure(request, response): void {
+		const name = request.params.name;
+		const type = request.params.type;
+		const extension = request.params.extension;
+		const promise = new Promise((resolve, reject) => {
+			fs.readFile(request.params.path, "utf8", (error, data) => {
+				resolve(data);
+			});
+		});
+		request.options.parentCtx.params.res.writeHead(200, {'Content-Type': 'text/plain'});
+		promise.then((data) => {
+			try {
 
-                request.options.parentCtx.params.res.end(JSON.stringify({
-                    data: {
-                        name,
-                        type,
-                        extension,
-                        fileData: data
-                    }
-                }));
-            }
-            catch (e) {
-                console.log(e);
-            }
-        });
-    }
+				request.options.parentCtx.params.res.end(JSON.stringify({
+					data: {
+						name,
+						type,
+						extension,
+						fileData: data
+					}
+				}));
+			} catch (e) {
+				console.log(e);
+			}
+		});
+	}
 
-    /**
-     * Метод рексрусивного спуска по директории проекта ,для формирование структуры проекта в виде массива объектов
-     * @param folder
-     * @param arrayOfStructures
-     * @returns {[Object]}
-     */
+	/**
+	 * Метод рексрусивного спуска по директории проекта ,для формирование структуры проекта в виде массива объектов
+	 * @param folder
+	 * @param arrayOfStructures
+	 * @returns {[Object]}
+	 */
 
-    readFolder(folder: string, arrayOfStructures: object[]): object[] {
-        const projectStructure = {};
-        let currentDirectory = fs.readdirSync(folder, 'utf8');
-        currentDirectory.forEach(file => {
-            let pathOfCurrentItem = path.join(folder, file);
-            const fileStats = fs.statSync(pathOfCurrentItem);
-            if (fileStats.isFile()) {
+	readFolder(folder: string, arrayOfStructures: object[]): object[] {
+		const projectStructure = {};
+		let currentDirectory = fs.readdirSync(folder, 'utf8');
+		currentDirectory.forEach(file => {
+			let pathOfCurrentItem = path.join(folder, file);
+			const fileStats = fs.statSync(pathOfCurrentItem);
+			if (fileStats.isFile()) {
+				if (this.checkFileExtension(path.extname(file))) {
+					arrayOfStructures.push({
+						name: file,
+						path: folder.concat('\/' + file),
+						extension: path.extname(file),
+						stats: fileStats,
+						type: 'file'
+					});
+				}
+			} else {
+				projectStructure[file] = {name: file, path: folder, type: 'directory', arrayOfStructures: []};
+				arrayOfStructures.push(projectStructure[file]);
+				this.readFolder(pathOfCurrentItem, projectStructure[file].arrayOfStructures);
+			}
+		});
+		return arrayOfStructures;
+	}
 
-                arrayOfStructures.push({
-                    name: file,
-                    path: folder.concat('\/' + file),
-                    extension: path.extname(file),
-                    stats:fileStats,
-                    type: 'file'
-                });
-            }
-            else {
-                projectStructure[file] = {name: file, path: folder, type: 'directory', arrayOfStructures: []};
-                arrayOfStructures.push(projectStructure[file]);
-                this.readFolder(pathOfCurrentItem, projectStructure[file].arrayOfStructures);
-            }
-        });
-        return arrayOfStructures;
-    }
+	/**
+	 * Метод проверки какие файлы нам ненужно отправлять клиенту
+	 * @param extension
+	 */
+	checkFileExtension(extension) {
+		return !extension.match(/\\*.(js)|\\*.(map)|\\*.(css)/);
+	}
 
+	/**
+	 * Метод отправки массива объектов со структурой директории клиенту
+	 * @param request
+	 */
+	sendDirectoryEngine(request: Context<object>): void {
+		const folder = `${globalVariables.serverFolderPath}/GameEngine/Client`;
+		const structure = this.readFolder(folder, [{}]);
 
-    /**
-     * Метод отправки массива объектов со структурой директории клиенту
-     * @param request
-     */
-    sendDirectoryEngine(request: Context<object>): void {
-        const folder = `${globalVariables.serverFolderPath}/GameEngine/Client`;
-        const structure = this.readFolder(folder, [{}]);
+		request.options.parentCtx.params.res.writeHead(200, {'Content-Type': 'text/plain'});
+		request.options.parentCtx.params.res.end(JSON.stringify({
+			data: [{
+				name: 'root',
+				type: 'directory',
+				arrayOfStructures: structure
+			}]
+		}));
+	}
 
-        request.options.parentCtx.params.res.writeHead(200, {'Content-Type': 'text/plain'});
-        request.options.parentCtx.params.res.end(JSON.stringify({
-            data: [{
-                name: 'root',
-                type: 'directory',
-                arrayOfStructures: structure
-            }]
-        }));
-    }
+	userAuthorization(ctx) {
+		const userParams = ctx.params;
+		return this.broker.call("DB.checkAuthorization", userParams).then(
+			result => {
+				return result;
+			},
+			error => {
+				console.log('Ошибка Авторизации пользователя DB')
+			}
+		);
+	}
 
-    userAuthorization(ctx) {
-        const userParams = ctx.params;
-        return this.broker.call("DB.checkAuthorization", userParams).then(
-            result => {
-                return result;
-            },
-            error => {
-                console.log('Ошибка Авторизации пользователя DB')
-            }
-        );
-    }
+	serviceCreated() {
+		this.logger.info("ES6 Service created.");
+	}
 
-    serviceCreated() {
-        this.logger.info("ES6 Service created.");
-    }
+	serviceStarted() {
+		this.logger.info("ES6 Service started.");
+	}
 
-    serviceStarted() {
-        this.logger.info("ES6 Service started.");
-    }
-
-    serviceStopped() {
-        this.logger.info("ES6 Service stopped.");
-    }
+	serviceStopped() {
+		this.logger.info("ES6 Service stopped.");
+	}
 }
 
 module.exports = EditorService;
